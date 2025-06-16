b110664b3c2d4d0477d92fac56e1b1d2f359c84f
JotaBarreto <barretojefferson@gmail.com>
2025-06-16 17:02:50 -0300

    Versão com credenciais locais

--- app.py
+++ app.py
@@ -2,42 +2,28 @@ from flask import Flask, jsonify, render_template
 import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import os
-import json
 from flask_cors import CORS
-from dotenv import load_dotenv
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-# Função para carregar as credenciais do Google a partir da variável de ambiente
+# Função para carregar as credenciais do arquivo local
 def get_google_credentials():
-    creds_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
-    print("Credenciais carregadas:", bool(creds_json))
-    
-    if not creds_json:
-        raise ValueError("Variável de ambiente GOOGLE_CREDENTIALS_JSON não encontrada")
-
-    creds_dict = json.loads(creds_json)
-    # CORRIGE o conteúdo da chave
-    creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
-
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    return ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-
+    return ServiceAccountCredentials.from_json_keyfile_name("credenciais.json", scope)
 
 # Função para carregar os dados do Google Sheets
 def carregar_dados():
     credenciais = get_google_credentials()
+    
     cliente = gspread.authorize(credenciais)
     planilha = cliente.open_by_key("1LyhymqJ8QKezAiR8ZCidyO9w2hKejP7F-IMFlBGyMJ8")
     aba = planilha.worksheet("Espelho")
+       
     dados = aba.get_all_records()
     df = pd.DataFrame(dados)
-
+    
     return df
-
 
 @app.route('/api/empreendimentos')
 def listar_empreendimentos():
@@ -52,10 +38,10 @@ def espelho(nome):
     df_filtrado.fillna('', inplace=True)
     resultado = df_filtrado.to_dict(orient='records')
     return jsonify(resultado)
-  
+
 @app.route('/')
 def index():
     return render_template('index.html')
-  
+
 if __name__ == '__main__':
     app.run(debug=True)
